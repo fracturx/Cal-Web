a24984e7855b1a9f8cfdb9612493c3ec163bb0f9
Fahim <fracturx@outlook.com>
2020-04-07 00:39:26 -0400

    configured the buttonz

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('fag')
+
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
